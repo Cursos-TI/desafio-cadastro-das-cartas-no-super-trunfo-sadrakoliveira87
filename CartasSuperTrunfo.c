c068f90c462456f6c7e030d15293ab8b745e1c5b
sadrakoliveira87 <sadrakoliveira87@gmail.com>
2025-03-01 18:08:00 -0300

    Update CartasSuperTrunfo.c
    desafio do primeiro tema entregue , cadastros das cartas, fiz uma linha pontilhada para dividir as cartas.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,14 +1,69 @@
 #include <stdio.h>
 
-
-
 int main() {
-    printf("desafio super trunfo!\n");
-    
-    char nome [30];
-    int força;
-    int velocidade;
-    int poder;
-    
-    return 0;
+	
+		char estado[1];
+		char codigo[20];
+		char nome[20];
+		int populacao;
+		float Area;
+		float PIB;
+		int pontos;
+		
+		printf("carta 1\n");
+		
+		printf ("Estado:", estado);
+		scanf("%s", &estado);
+		
+		printf("codigo da carta:", codigo);
+		scanf("%s", &codigo);
+		
+		printf("nome da cidade:", nome);
+		scanf("\n%20[^\n]s", nome);
+		
+		printf("populacao:", populacao);
+		scanf("%d", &populacao);
+		
+		printf("Area em km²:", Area);
+		scanf("%f", &Area);
+		
+		printf("PIB:", PIB);
+		scanf("%f", &PIB);
+		
+		printf("pontos turisticos:", pontos);
+		scanf("%d", &pontos);
+		
+		printf("\n---------------------\n\n");
+		
+		printf("carta 2\n");
+		
+		printf ("Estado:", estado);
+		scanf("%s", &estado);
+		
+		printf("codigo da carta:", codigo);
+		scanf("%s", &codigo);
+		
+		printf("nome da cidade:", nome);
+		scanf("\n%20[^\n]s", nome);
+		
+		printf("populacao:", populacao);
+		scanf("%d", &populacao);
+		
+		printf("Area em km²:", Area);
+		scanf("%f", &Area);
+		
+		printf("PIB:", PIB);
+		scanf("%f", &PIB);
+		
+		printf("pontos turisticos:", pontos);
+		scanf("%d", &pontos);
+		
+		
+		
+		
+	
+		
+
+
+	return 0;
 }
